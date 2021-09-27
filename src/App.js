2e80d4cb5fe666c5bfdf27411a112de35219b956
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-09-26 17:49:12 -0700

    Initial set-up
    Created the react app that will be deployed on GitHub pages.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import BudgetCalculator from './BudgetCalculator.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Budget Calculator.....</h1>
+      </header>
+      <BudgetCalculator />
+    </div>
+
+  );
+}
+
+export default App;
