b35fbace1518e61d156ab193ec2795b9964664ad
BECKA SEEVERS <BECKASEEVERS@U.BOISESTATE.EDU>
2021-10-07 12:53:53 -0600

    #8 scenario

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import Scenario from './Scenario.js';
+import './Main.css';
+
+function Main() {
+    return (
+        <div>
+            <Scenario />
+        </div>
+    );
+}
+
+export default Main;
