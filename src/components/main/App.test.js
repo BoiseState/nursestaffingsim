1c8c8466880abd7c69e429d472abd299fe02c64a
ilove <ilove@LAPTOP-ADU6V17G>
2021-10-09 06:40:46 -0600

    Delete redundant code

--- src/components/main/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AppInMain from './AppInMain';
-
-test('renders learn react link', () => {
-    render(<AppInMain />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
