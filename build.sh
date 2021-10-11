202da6bdb23a99ccc16f3b0dd6b8e75e5bcd6acb
ilove <ilove@LAPTOP-ADU6V17G>
2021-10-10 20:57:33 -0600

    Add a command

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npm install || exit 127
+npm install antd --save || exit 127
 npm run build || exit 127
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npm install || exit 127
+npm install antd --save || exit 127
 npm run test a || exit 127
