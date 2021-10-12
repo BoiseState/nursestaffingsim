0aa4d1b476706d982ef83ab313a93e2bd1907c26
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-10-11 20:37:21 -0700

    Update App.test.js
    Left out the scenario field added it to the render tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Header from './components/header/Header.js';
 import Footer from './components/footer/Footer.js';
+import Main from './components/main/MAin.js';
 
 
 test('Main page renders', () => {
@@ -19,6 +20,13 @@ render(<Header />);
   const HeaderElement = screen.getByText(/BSU In-Patient Nurse Staffing Simulator/i);
   expect(HeaderElement).toBeInTheDocument();
 });
+
+test('Scenario content renders', () => {
+  render(<Main />);
+  const HeaderElement = screen.getByText(/allocate your staffing resources/i);
+  expect(HeaderElement).toBeInTheDocument();
+});
+
 
 //Will need to be uncommented when merge happens
 // test('Staff content renders', () => {
