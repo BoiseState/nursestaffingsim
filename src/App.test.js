0c00374ff846732dad6ff614ffc02f3170e828e4
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-10-11 20:22:44 -0700

    Added the three rendering tests for components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/header/Header.js';
+import Footer from './components/footer/Footer.js';
 
-test('renders page', () => {
+
+test('Main page renders page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/BSU In-Patient Nurse Staffing Simulator/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('Footer renders', () => {
+  render(<Footer />);
+  const FooterElement = screen.getByText(/BSU Nursing | Dr. Renee Walters | reneewalters@boisestate.edu/i);
+  expect(FooterElement).toBeInTheDocument();
+});
+
+test('Header renders', () => {
+render(<Header />);
+  const HeaderElement = screen.getByText(/BSU In-Patient Nurse Staffing Simulator/i);
+  expect(HeaderElement).toBeInTheDocument();
+});
+
+//Will need to be uncommented when merge happens
+// test('Staff content renders', () => {
+//   render(<AppInMain />);
+// });
