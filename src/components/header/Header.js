33a12cd5e09866707a12327745b9eb64d966b952
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-11-01 22:40:08 -0600

    Added a container around the page and then changed up the header to add a NAV along with the title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import './Header.css';
 import React from 'react';
 import logo from '../../assets/Left-aligned-square-gray-2.png';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class Header extends React.Component {
 
@@ -11,11 +10,32 @@ class Header extends React.Component {
     }
 
     render() {
-        return(
-            <Jumbotron id="header">
-                <img src={logo} alt="Logo" data-testid="reload" onClick={this.onReload}/>
-                <h1>BSU In-Patient Nurse Staffing Simulator</h1>
-            </Jumbotron>
+        return (
+
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <img src={logo} alt="Logo" height="50" data-testid="reload" onClick={this.onReload} />
+
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="https://www.boisestate.edu/nursing/">Nursing Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="p-5 bg-primary text-white rounded">
+                    <h1>In-Patient Staffing Simulator</h1>
+                </div>
+            </header>
+
         );
     }
 }
